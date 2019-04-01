6dbb0d735c4c5d272f72fd9537307853fdd0a49b
pegasus-lynx <dipeshkrj14@gmail.com>
2019-04-01 20:17:51 +0530

    Connected app to database

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'exploapi',
+        'USER': 'explo',
+        'PASSWORD': 'explo',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
